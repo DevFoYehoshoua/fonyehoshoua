3efc1db7cd4ce312d98ecf087abe75a1efa58fb0
Zivan-R <135060460+Zivan-R@users.noreply.github.com>
2025-02-14 12:59:16 +0100

    changed vite.config for deployment on domain name

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/foniyah/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/fonyehoshoua/' : '/',
+  base: '/',
 
   plugins: [vue()],
   resolve: {
