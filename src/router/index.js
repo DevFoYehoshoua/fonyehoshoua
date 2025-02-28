199a76fb525f8639b577013a8735975f2d28310c
Zivan-R <135060460+Zivan-R@users.noreply.github.com>
2025-02-28 16:26:15 +0100

    après modif dans i18n.js, ajustement router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   // history: createWebHashHistory(import.meta.env.BASE_URL),
   history: createWebHashHistory(),
   routes: [
-    { path: '/', name: 'home', component: HomeView }
+    { path: '/:lang(fr|mg)?', name: 'home', component: HomeView }
     // {
     //   path: '/about',
     //   name: 'about',
